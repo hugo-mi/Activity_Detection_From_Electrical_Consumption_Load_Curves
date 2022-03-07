8edc949e6a85c1ab759704855927278f888b8494
pierre-db <pierre.dalbianco@free.fr>
2022-03-07 18:20:12 +0100

    improve module by adding the plot_scores_param function

--- Exploratory Data Analysis/module.py
+++ Exploratory Data Analysis/module.py
@@ -1,7 +1,11 @@
 from typing import List, Set, Dict, Tuple, Optional, Any
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import fbeta_score, accuracy_score
 
 def load_dataset(filename: str, resample_period :Optional[str]=None) -> pd.DataFrame:
     """
@@ -78,3 +82,53 @@ def generate_scaled_features(data: pd.DataFrame, window: Optional[str]='1h', sca
     features_col = ['mains_scaled', 'hour_scaled', 'std_'+window+'_scaled', 'mean_'+window+'_scaled', 'maxmin_'+window+'_scaled']
 
     return data, features_col
+
+def plot_scores_param(X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray,
+                      estimator: Any, param_name: str, param_range: List[float], other_params: Optional[Dict[str, Any]]={}) -> Tuple[str, float, float]:
+    """
+    Performs a grid search on a model on a single parameter and displays the scores vs parameter
+    X_train: the features to train the model on
+    y_train: the labels to train the model
+    X_test: the features to evaluate the model on
+    y_test: the labels to evaluate the model
+    estimator: the estimator to fit
+    param_name: the name of the parameter on which we perform the grid search
+    param_range: the range of the parameter to perform the grid search
+    other_params: (optional) additional parameters to pass to the classifier
+    returns: the parameter name, its the best value, the accuracy and the f2 score associated with the best value
+    """
+    f2_score = []
+    score = []
+    for p in param_range:
+        classifier = estimator(**{param_name:p}, **other_params)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+
+        f2_score.append((fbeta_score(y_test, y_pred, average='macro', beta=0.5)))
+        score.append(accuracy_score(y_test, y_pred))
+
+        print(f"tested {param_name}={p} ...")
+
+    best_param = np.argmax(f2_score)
+
+    plt.figure(figsize=(10, 6));
+    plt.plot(param_range, score, label='score', color='grey', linestyle='dashed');
+    plt.plot(param_range, f2_score, label='fb score');
+    plt.scatter(param_range[best_param], f2_score[best_param], label='fb max', marker='x', s=100, color='red')
+    plt.legend();
+    plt.title('fb and score = f({})'.format(param_name));
+    plt.show();
+
+    print('Meilleur fb score={:.2f} obtenu pour {}={:.2f}'.format(f2_score[best_param], param_name, param_range[best_param]))
+
+    classifier = estimator(**{param_name:param_range[best_param]})
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+
+    sns.heatmap(pd.crosstab(y_test, y_pred, normalize=True), annot=True, fmt='.1%', vmin=0, vmax=1, cmap=sns.cm.rocket_r);
+    plt.xlabel('vérité');
+    plt.ylabel('prédiction');
+    plt.title('Resultats avec {}={:.2f}'.format(param_name, param_range[best_param]));
+    plt.show();
+
+    return param_name, param_range[best_param], score[best_param], f2_score[best_param]
