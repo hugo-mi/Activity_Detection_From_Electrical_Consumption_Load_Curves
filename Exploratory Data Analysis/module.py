3084484c86953b3e2cb150931ee2aacbedaba22d
pierre-db <pierre.dalbianco@free.fr>
2022-03-07 18:02:36 +0100

    add module with commonly used functions

---
+++ Exploratory Data Analysis/module.py
@@ -0,0 +1,80 @@
+from typing import List, Set, Dict, Tuple, Optional
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def load_dataset(filename: str, resample_period :str=None) -> pd.DataFrame:
+    """
+    Loads the dataset
+    filename: the path to the file to load
+    resample_period: (optional) the reasmple period, if None the default period of 1 second will be used
+    returns: a DataFrame containing the dataset
+    """
+    dataset = pd.read_csv(filename, index_col='datetime').interpolate('linear')
+    dataset.index = pd.to_datetime(dataset.index)
+    dataset = dataset.asfreq('s')
+
+    if resample_period:
+        dataset = dataset.resample(resample_period).nearest()
+    
+    dataset['hour'] = dataset.index.hour
+
+    return dataset
+
+def pick_random_indexes(data: pd.DataFrame, percentage: float=0.3) -> pd.DatetimeIndex:
+    """
+    Returns random indexes from the index of the DataFrame passed in parameter
+    data: the DataFrame to use
+    percentage: the percentage of indexes to randomly pick
+    returns: a DatetimeIndex with random dates
+    """
+    # tirage de jours aléatoires
+    delta_time = data.index[-2] - data.index[0]
+    nb_days = int(delta_time.days * percentage)
+    random_dates = data.index[0] + pd.to_timedelta(np.random.choice(delta_time.days, nb_days, replace=False), unit='day')
+
+    data_freq = pd.Timedelta(data.index.freq).seconds if data.index.freq else 1
+    # définition des indexes test, train
+    rand_indexes = pd.DatetimeIndex(np.array([pd.date_range(d, periods=24*60*60/data_freq, freq=data.index.freq) for d in random_dates]).ravel())
+
+    return rand_indexes
+
+def split_train_test_indexes(data: pd.DataFrame, percentage: float=0.3) -> Tuple[pd.DatetimeIndex]:
+    """
+    Performs a split train test on a time series by picking random full days
+    data: the DataFrame to use
+    percentage: the percentage of indexes to randomly pick for the test
+    returns: a tuple of DatetimeIndex with random days for the train indexes and test indexes
+    """
+    # tirage de jours aléatoires
+    delta_time = data.index[-2] - data.index[0]
+    nb_days = int(delta_time.days * percentage)
+    random_dates = data.index[0] + pd.to_timedelta(np.random.choice(delta_time.days, nb_days, replace=False), unit='day')
+
+    data_freq = pd.Timedelta(data.index.freq).seconds if data.index.freq else 1
+    # définition des indexes test, train
+    test_indexes = pd.DatetimeIndex(np.array([pd.date_range(d, periods=24*60*60/data_freq, freq=data.index.freq) for d in random_dates]).ravel())
+    train_indexes = data.index[~np.isin(data.index, test_indexes)]
+
+    return train_indexes, test_indexes
+
+def generate_scaled_features(data: pd.DataFrame, window: str='1h', scaler=StandardScaler(), fillna_method :str='bfill') -> Tuple[pd.DataFrame, List[str]]:
+    """
+    Generates features for classifications
+    data: the DataFrame to use
+    window: (optional) the window of time for the rolling transformations
+    scale: (optional) the scaler to use
+    fillna_method: (optional) the method to use the fill na values that will occure due to the rolling transformations
+    returns: a DataFrame with the old and new features and the list containing the names of the new features columns
+    """
+    # we prepare our features
+    data['mains_scaled'] = scaler.fit_transform(data['mains'].values.reshape(-1,1))
+    data['mean_'+window+'_scaled'] = scaler.fit_transform(data['mains'].rolling(window).mean().values.reshape(-1,1))
+    data['std_'+window+'_scaled'] = scaler.fit_transform(data['mains'].rolling(window).std().values.reshape(-1,1))
+    data['maxmin_'+window+'_scaled'] = scaler.transform(data['mains'].rolling(window).max().values.reshape(-1,1) - data['mains'].rolling(window).min().values.reshape(-1,1))
+    data['hour_scaled'] = scaler.fit_transform(data['hour'].values.reshape(-1,1))
+    data = data.fillna(method=fillna_method)
+
+    features_col = ['mains_scaled', 'hour_scaled', 'std_'+window+'_scaled', 'mean_'+window+'_scaled', 'maxmin_'+window+'_scaled']
+
+    return data, features_col
